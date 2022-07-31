0b25a4179c4d34f26583dc426b68ecc924ffb405
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2022-07-31 20:12:14 +0530

    Time: 2 ms (89.30%), Space: 56.4 MB (35.01%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,11 +1,36 @@
 class Solution {
     public int majorityElement(int[] a) {
-        HashMap<Integer, Integer> m = new HashMap<>();
-        int size = a.length;
-        for(int i = 0; i < size; i++) {
-            m.put(a[i], m.getOrDefault(a[i], 0)+1);
-            if(m.get(a[i]) > size/2) return a[i];
+        
+        //Moore Voting Algo
+        int candidate = -1, count = 0;
+        for(int i = 0; i < a.length; i++) {
+            if(count == 0) {
+                candidate = i;
+            }
+            
+            count += (a[candidate] == a[i]) ? 1:-1;
         }
-        return -1;
+        
+        //If it were not assured that Maj exists
+        //Code to check:
+        
+        // count= 0;
+        // for (int i = 0; i < a.length; i++) {
+        //     if(a[candidate] == a[i])    count ++;
+        // }
+        // if(count <= a.length/2)
+        //     candidate = -1;
+        
+        return a[candidate];
+        
+        //Hashing Method
+        
+        // HashMap<Integer, Integer> m = new HashMap<>();
+        // int size = a.length;
+        // for(int i = 0; i < size; i++) {
+        //     m.put(a[i], m.getOrDefault(a[i], 0)+1);
+        //     if(m.get(a[i]) > size/2) return a[i];
+        // }
+        // return -1;
     }
 }
