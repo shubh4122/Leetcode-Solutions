a11fcbc81be1dd93679a1800868d2970691a00df
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2022-09-27 22:38:31 +0530

    Time: 4 ms (60.79%), Space: 41.2 MB (56.26%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -5,15 +5,38 @@ class Solution {
             arr.add(i+1);
         }
 
+        
+//-------------------ITERATIVE -------------------
+        
 //        this is current position
-        int curr = 0;
-        while (arr.size() > 1) {
-            int killAt = (curr + k - 1) % arr.size();
+//         int curr = 0;
+//         while (arr.size() > 1) {
+//             int killAt = (curr + k - 1) % arr.size();
 
-            arr.remove(killAt);
-            curr = killAt;
-        }
+//             arr.remove(killAt);
+//             curr = killAt;
+//         }
 
-        return arr.get(0);
+//         return arr.get(0);
+        
+        
+        
+//-------------------RECURSIVE-------------------
+        return helper(arr, k, 0);
+        
+    }
+    
+    public int helper(ArrayList<Integer> arr, int k, int curr) {
+        //        H : jPR(arr, k, curr) --> returns safe posn in the circle
+//        I : jPR(arr, k, curr) --> arr.remove(killat)
+//                                  jPR(arr, k, killat)
+//        BC : if arr.size == 1, return arr[0]
+
+        if (arr.size() == 1)
+            return arr.get(0);
+
+        int killAt = (curr + k - 1) % arr.size();
+        arr.remove(killAt);
+        return helper(arr, k, killAt);
     }
 }
