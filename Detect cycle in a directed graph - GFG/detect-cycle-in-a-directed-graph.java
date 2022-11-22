78fadfd288c610a6d0956a2d5dc0c4aa6dcab08a
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2022-11-23 01:48:06 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -34,38 +34,54 @@ class DriverClass {
 class Solution {
     // Function to detect cycle in a directed graph.
     public boolean isCyclic(int n, ArrayList<ArrayList<Integer>> graph) {
-        // code here
-        boolean visited[] = new boolean[n];
-        boolean dfsVisited[] = new boolean[n];
+        //      Using Kahn's algo
+//      SAME CODE as topoBFS. almost
 
+//      Data structures Required
+        int[] indegree = new int[n];
+        int[] ans = new int[n];
+        Queue<Integer> q = new ArrayDeque<>();
+
+//      Calculating INDEGREE
         for (int node = 0; node < n; node++) {
-            if (!visited[node])
-                if (checkCycleDFS(graph, node, visited, dfsVisited))
-                    return true;
-        }
-        return false;
-    }
-    
-    public static boolean checkCycleDFS(ArrayList<ArrayList<Integer>> graph, int node, boolean[] visited, boolean[] dfsVisited) {
-//      No Base Condtion here !! IMP HERE
-//      Whenever any node is visited. mark both arrays as visited!
-        visited[node] = true;
-        dfsVisited[node] = true;
-
-//       Now Check each node's Adj nodes, to have both vis = dfsVis = true
-        for (int adjNode : graph.get(node)) {
-            if (!visited[adjNode]) {
-                if (checkCycleDFS(graph, adjNode, visited, dfsVisited))
-                    return true;
+            for (int adjNode : graph.get(node)) {
+                indegree[adjNode]++;
             }
-//          This condition means, vis[adj] = true, and dfsvis[adj] is also TRUE. HENCE CYCLE FOUND
-            else if (dfsVisited[adjNode] == true)
-                return true;
         }
 
-//      SEE WHAT IT DOES??
-        dfsVisited[node] = false;
-        return false;
+//      whosever in-deg is 0, put them in Queue
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0)
+                q.add(i);
+        }
+
+//      Starting the BFS now.
+        int i = 0;
+//      This count variable keeps count of how many nodes were popped out of queue
+        int count = 0;
+        while (!q.isEmpty()) {
+//          pop the front of Queue. and store it.
+            int temp = q.remove();
+            ans[i] = temp;
+            i++;
+//          increment count with each pop. To count nodes
+            count++;
+
+//          Traverse all its Adj nodes
+            for (int adjNode : graph.get(temp)) {
+                if (indegree[adjNode] != 0) { // THIS is REDUNDANT. Coz, this will always be >= 0. this is simply the count of nodes pointing to this node. THINK!
+                    indegree[adjNode]--;
+                    if (indegree[adjNode] == 0)
+                        q.add(adjNode);
+                }
+            }
+        }
+
+//      If count(or no. of popped nodes = n) then topo sort sequence generated, else not
+        if (count == n) {
+            return false;
+        }
+        return true;
     }
 
 
