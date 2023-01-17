1a55c70e5ce1e6db3d47ea3c7593cff1f14df534
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2023-01-18 02:05:30 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.java
+++ Minimum sum partition - GFG/minimum-sum-partition.java
@@ -43,27 +43,28 @@ class Solution
         for (int i = 0; i < dpMem.length; i++)
             Arrays.fill(dpMem[i], -1);
             
-        int sumPartition1 = maxSumNearestToHalfSum(arr, n, halfSum, dpMem);
+        int sumPartition1 = ksMem(arr, arr, halfSum, n, dpMem);
         int sumPartition2 = sum - sumPartition1;
 
         return Math.abs(sumPartition2 - sumPartition1);
 	} 
 	
-	//This function returns SUM nearest to given sum
-    private static int maxSumNearestToHalfSum(int[] arr, int n, int sum, int[][] dpMem) {
+    
+        public static int ksMem(int[] val, int[] wt, int w, int n, int[][] dpMem) {
         //BC
-        if (sum == 0 || n == 0)   return 0;
+        if (n == 0 || w == 0)
+            return 0;
 
-        //Memoization
-        if (dpMem[n][sum] != -1)
-            return dpMem[n][sum];
+        //Just Recursive solution Takes too much time. so use MEMOIZATION
+        if (dpMem[n][w] != -1)
+            return dpMem[n][w];
 
-        //CODE(Choice Diag)
-        //Just like knapsack, Return Max Sum(profit there) nearest to Given SUM(W there)
-        if (arr[n-1] <= sum)
-            return dpMem[n][sum] = Math.max(arr[n-1] + maxSumNearestToHalfSum(arr, n-1, sum - arr[n-1], dpMem),
-                                            maxSumNearestToHalfSum(arr, n-1, sum, dpMem));
+        //Choice Diag
+        if (wt[n-1] <= w)
+            return dpMem[n][w] = Math.max(val[n-1] + ksMem(val, wt, w - wt[n-1], n-1, dpMem),
+                    ksMem(val, wt, w, n-1, dpMem));
 
-        else return dpMem[n][sum] = maxSumNearestToHalfSum(arr, n-1, sum, dpMem);
+        else //if wt[n-1] > w
+            return dpMem[n][w] = ksMem(val, wt, w, n-1, dpMem);
     }
 }
