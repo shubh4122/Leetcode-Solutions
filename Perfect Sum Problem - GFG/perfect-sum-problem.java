9a28f9aea264b5181f43d9dc91544278813e379e
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2023-01-17 22:56:24 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -30,36 +30,29 @@ class Solution{
 	public int perfectSum(int arr[],int n, int sum) 
 	{ 
 	    // Your code goes here
-	    
-        int[][] dpMem = new int[n+1][sum+1];
-        for (int i = 0; i < dpMem.length; i++)
-            Arrays.fill(dpMem[i], -1);
-        return countSubsetsMem(arr, n, sum, dpMem);
-	    
+	    int[][] dp = new int[n+1][sum+1];
+        //BC
+        Arrays.fill(dp[0], 0);
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (arr[i-1] <= j)
+                    //count of subsets equal to given sum, when including (n-1)th item in arr
+                    //+
+                    // count of subsets whose sum = given sum, when not including (n-t)th elem
+                    dp[i][j] = mod1000000007(dp[i-1][j - arr[i-1]] + dp[i-1][j]);
+
+                else    dp[i][j] = dp[i-1][j];
+
+            }
+        }
+
+        return dp[n][sum];
 	} 
 	
-	public int countSubsetsMem(int[] arr, int n, int sum,int[][] dpMem) {
-        //BC
-        if (n == 0 && sum != 0)
-            return 0; //No subset possible. hence count = 0
-
-        if (n == 0 && sum == 0)
-            return 1;//empty set possible
-
-        //Memoization
-        if (dpMem[n][sum] != -1)
-            return dpMem[n][sum];
-
-        //Main code (Choice Diag)
-        if (arr[n-1] <= sum)
-            return dpMem[n][sum] = mod1000000007(countSubsetsMem(arr, n-1, sum - arr[n-1], dpMem) //count of subsets equal to given sum, when including (n-1)th item in arr
-                                +
-                    countSubsetsMem(arr, n - 1, sum, dpMem)); // count of subsets whose sum = given sum, when not including (n-t)th elem
-
-        else    return dpMem[n][sum] = mod1000000007(countSubsetsMem(arr, n - 1, sum, dpMem));
-    }
-    
-        public static int mod1000000007(long n) {
+	
+    public static int mod1000000007(long n) {
         return (int)(n%(1e9+7));
     }
 }
