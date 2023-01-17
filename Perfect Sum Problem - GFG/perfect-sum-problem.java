8a8a61e02f5921b5206df86e6cb63a9d6b5f900e
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2023-01-17 18:47:46 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -45,9 +45,9 @@ class Solution{
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < sum + 1; j++) {
                 if (arr[i-1] <= j)
-                    dp[i][j] = mod1000000007(mod1000000007(dp[i - 1][j - arr[i - 1]]) + mod1000000007(dp[i - 1][j]));//||(or) replaced by +
+                    dp[i][j] = mod1000000007(dp[i - 1][j - arr[i - 1]] + dp[i - 1][j]);//||(or) replaced by +
                 else
-                    dp[i][j] = mod1000000007(dp[i-1][j]);
+                    dp[i][j] = dp[i-1][j];
             }
         }
         return mod1000000007(dp[n][sum]);
