fdfea582e4bc7867d075a3f0d973bd913c498000
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2023-03-22 02:27:36 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -36,66 +36,77 @@ public class Main{
 class Solution{
 	static int spanningTree(int n, int m, int edges[][]){
 	    // Code Here. 
-	    ArrayList<ArrayList<Pair>> graph = edgeToListWeighted(edges, n, m);
+	    //Step1 - sort edges about wt
+        Arrays.sort(edges, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];//index [i][2] is weight of corresponding edge
+            }
+        });
+        
+        //Step 2: use disjoint set, and make connections
+        DisjointSet ds = new DisjointSet(n);
         int mstWt = 0;
-        PriorityQueue<Pair> q = new PriorityQueue<>(new Pair());//comparator
-        boolean[] vis = new boolean[n];
 
-        q.add(new Pair(0, 0));//any random node pushed
-
-        while (!q.isEmpty()) {
-            Pair pop = q.remove();
-            int node = pop.first;
-            int wt = pop.second;
-
-
-            //this is because there may be multiple occurences of a node in PQ
-            //so, once the smallest weight pair of that node is visited, all others ignored
-            //using this if.
-            if (vis[node])  continue;
-            //taking the shortest edge
-            mstWt += wt;
-            vis[node] = true;//mark nodes visited only when they are popped and included in MST
-
-            for (Pair adj : graph.get(node)) {
-                //When nodes are entered here, it doesnt mean it is VISITED.
-                //only when we pop them, they are marked visited.
-                //Hence same node with different weights maybe present in PQ
-                if (!vis[adj.first]) {
-                    q.add(adj);
-                }
+        for (int[] edge : edges) {
+            if (ds.findUltParent(edge[0]) != ds.findUltParent(edge[1])) {
+                mstWt += edge[2];
+                ds.unionByRank(edge[0], edge[1]);
             }
         }
         return mstWt;
 	}
 	
 	
-    public static ArrayList<ArrayList<Pair>> edgeToListWeighted(int[][] edges, int n, int m) {
-        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {//<=n because 1 based indexing
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < m; i++) {
-            graph.get(edges[i][0]).add(new Pair(edges[i][1], edges[i][2]));
-            graph.get(edges[i][1]).add(new Pair(edges[i][0], edges[i][2]));
-        }
-
-        return graph;
-    }
+    static class DisjointSet {
+        ArrayList<Integer> rank = new ArrayList<>();
+        ArrayList<Integer> parent = new ArrayList<>();
     
-     public static class Pair implements Comparator<Pair> {
-        int first, second;
-        public Pair(){}
-
-        public Pair(int first, int second){
-            this.first = first;
-            this.second = second;
+        //made public for it to be accessible outside package
+        public DisjointSet(int numOfNodes) {
+            for (int i = 0; i < numOfNodes + 1; i++) { // +1 to support 1 based indexing too
+                rank.add(0); //Default values
+                parent.add(i);
+            }
         }
-
-        @Override
-        public int compare(Pair p1, Pair p2) {
-            return p1.second - p2.second;//ascending order
+    
+        public int findUltParent(int node) {//finding ultimate parent
+            if (node == parent.get(node))
+                return node;
+    
+            int ulp = findUltParent(parent.get(node));
+            //it REPLACES val at index node with val ULP
+            parent.set(node, ulp);
+            return parent.get(node);
+        }
+    
+        public void unionByRank(int u, int v) {
+    /*
+             ----------------------------------------------
+            |                 Union Algo                   |
+            |     1. Find ult Parent of u,v (pu, pv)       |
+            |     2. Find RANK of pu, pv                   |
+            |     3. Attach smaller rank ultimate parent   |
+            |        to larger Rank ultimate Par           |
+             ----------------------------------------------
+     */
+            int pu = findUltParent(u);
+            int pv = findUltParent(v);
+    
+            //if pu and pv of both same, then no need to union them
+            if (pu == pv)   return;
+    
+            if (rank.get(pu) < rank.get(pv)) {
+                parent.set(pu, pv); // Make pv as Parent of pu, That is attach pu to pv.
+                //Rank of pv doesnt change coz it was already higher. so it wouldnt cause any change
+            }
+            else if (rank.get(pu) > rank.get(pv)) {
+                parent.set(pv, pu);
+            }
+            else {//when both rank equal, then join any to any, and increase RANK of final UltPar
+                parent.set(pv, pu);
+                rank.set(pu, rank.get(pv)+1);//or rank[pu] + 1 will be same
+            }
         }
     }
 }
