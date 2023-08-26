c4a74daec8430e432b1048cd52c586a9e242b9d2
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2023-08-27 00:00:05 +0530

    Added solution - LeetHub

--- Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.java
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.java
@@ -28,46 +28,43 @@ class GFG {
 class Solution {
 
     int search(String p, String s) {
-        int st= 0, e = 0;
-        HashMap<Character, Integer> mapP = new HashMap<>();
-        HashMap<Character, Integer> mapS = new HashMap<>();
+        // code here
+        int st = 0, e = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
 
         //store freq of characters in P
         for (int i = 0; i < p.length(); i++) {
-            mapP.put(p.charAt(i), mapP.getOrDefault(p.charAt(i), 0)+1);
+            map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) + 1);
         }
 
-        int countAnagrams = 0;
-        while (e < s.length()){
-            mapS.put(s.charAt(e), mapS.getOrDefault(s.charAt(e), 0)+1);//1
-
-            if (e-st+1 == p.length()) {//2
-                if (checkForAnagrams(mapP, mapS))//2.1
-                    countAnagrams++;
-                
-                mapS.put(s.charAt(st), mapS.get(s.charAt(st))-1);//2.2
-                if (mapS.get(s.charAt(st))==0)
-                    mapS.remove(s.charAt(st));
-                    
-                st++;//2.3
+        // List<Integer> ans = new ArrayList<>();
+        int ans = 0;
+        int countDistinctLetters = map.size();//We keep track of total distinct elem in map
+        while (e < s.length()) {
+            //step1
+            if (map.containsKey(s.charAt(e))) {
+                map.put(s.charAt(e), map.get(s.charAt(e)) - 1);
+                if (map.get(s.charAt(e)) == 0)
+                    countDistinctLetters--;
             }
 
-            e++;//3
+            //Step2
+            if (e - st + 1 == p.length()) {
+                //2.1
+                if (countDistinctLetters == 0)
+                    ans++;
+                //2.2
+                if (map.containsKey(s.charAt(st))) {
+                    map.put(s.charAt(st), map.get(s.charAt(st)) + 1);
+                    if (map.get(s.charAt(st)) == 1)
+                        countDistinctLetters++;
+                }
+                //2.3
+                st++;
+            }
+            //Step3
+            e++;
         }
-        return countAnagrams;
-    }
-
-    private boolean checkForAnagrams(HashMap<Character, Integer> mapP, HashMap<Character, Integer> mapS) {
-        if (mapP.size() != mapS.size())
-            return false;
-
-        for (Map.Entry<Character, Integer> entry : mapP.entrySet()) {
-            char key = entry.getKey();
-            int count = entry.getValue();
-
-            if (mapS.getOrDefault(key, 0) != count)
-                return false;
-        }
-        return true;
+        return ans;
     }
 }
