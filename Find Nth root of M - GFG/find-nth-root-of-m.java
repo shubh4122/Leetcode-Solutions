91cd8d47ee0b5b81d9bf0416b939b9402d069013
Shubham Pandey <86017598+shubh4122@users.noreply.github.com>
2023-09-21 12:28:18 +0530

    Added solution - LeetHub

---
+++ Find Nth root of M - GFG/find-nth-root-of-m.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            String[] S1 = s.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int m = Integer.parseInt(S1[1]);
+            Solution ob = new Solution();
+            int ans = ob.NthRoot(n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int NthRoot(int n, int m)
+    {
+        // code here
+        int s = 0, e = m;
+        int ans = -1;
+        while(s <= e) {
+            int mid = s + (e-s)/2;
+            
+            if(Math.pow(mid, n) == m){
+                ans = mid;
+                break;
+            }
+            
+            if(Math.pow(mid, n) > m)
+                e = mid-1;
+                
+            else
+                s = mid+1;
+        }
+        return ans;
+    }
+}
